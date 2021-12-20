0485281a17b04c7b1e0c0d74de51be1fd4360913
Love AVL <51767480+Spartak-Belov-Floresku@users.noreply.github.com>
2021-12-20 01:43:11 -0500

    Update jobly-seed.sql

--- backend/jobly-seed.sql
+++ backend/jobly-seed.sql
@@ -1,18 +1,12 @@
--- both test users have the password "password"
+-- test user has the password "password"
 
 INSERT INTO users (username, password, first_name, last_name, email, is_admin)
 VALUES ('testuser',
         '$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q',
         'Test',
         'User',
-        'joel@joelburton.com',
-        FALSE),
-       ('testadmin',
-        '$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q',
-        'Test',
-        'Admin!',
-        'joel@joelburton.com',
-        TRUE);
+        'testuser@testuser.com',
+        FALSE);
 
 INSERT INTO companies (handle,
                        name,
